ffd71c5c097bd7899f310133f38b0724effaeb19
Chas Nelson <github@chasnelson.co.uk>
2024-03-01 16:43:36 +0000

    feat: add csv_file_stub to taxonomy API for easier use in bahis-desk

--- bahis_management/taxonomies/api/serializers.py
+++ bahis_management/taxonomies/api/serializers.py
@@ -7,9 +7,11 @@ from rest_framework import serializers
 
 
 class TaxonomySerializer(serializers.ModelSerializer):
+    csv_file_stub = serializers.FileField(use_url=False, read_only=True, source="csv_file")
+
     class Meta:
         model = Taxonomy
-        fields = ["id", "title", "slug", "csv_file"]
+        fields = ["id", "title", "slug", "csv_file", "csv_file_stub"]
 
 
 class AdministrativeRegionLevelSerializer(serializers.ModelSerializer):
