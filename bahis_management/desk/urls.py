5bcb91f4ab8ad60329fe56874667bd9aa9581882
Chas Nelson <github@chasnelson.co.uk>
2024-03-28 06:37:37 +0000

    fix: correct url namespace typo (#40)
    * fix: correct url namespace typo
    
    * fix: remove trailng slash
    
    * fix: sort out local and cloud fonts to reduce warning messages in logs
    
    * fix: correct urls order for correct routing
    
    * fix: ensure workflows create button goes to workflows and not modules create form
    
    * chore: format

--- bahis_management/desk/urls.py
+++ bahis_management/desk/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path("create/", views.ModuleCreate.as_view(), name="create"),
     path("update/<int:pk>/", views.ModuleUpdate.as_view(), name="update"),
     path("delete/<int:pk>/", views.ModuleDelete.as_view(), name="delete"),
-    path("workflows/<str:source_form>/", views.WorkflowList.as_view(), name="workflows_list"),
     path("workflows/create/", views.WorkflowCreate.as_view(), name="workflows_create"),
     path("workflows/update/<int:pk>/", views.WorkflowUpdate.as_view(), name="workflows_update"),
     path("workflows/delete/<int:pk>/", views.WorkflowDelete.as_view(), name="workflows_delete"),
+    path("workflows/<str:source_form>/", views.WorkflowList.as_view(), name="workflows_list"),
 ]
