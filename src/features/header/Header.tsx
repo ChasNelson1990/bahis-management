3196439c69a1a60b373fffe897224923547c2a26
Sharful <sharful09@gmail.com>
2024-09-08 13:10:34 +0600

    fix: base url

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -52,7 +52,7 @@ function Header() {
                         variant="h6"
                         noWrap
                         component="a"
-                        href="#app-bar-with-responsive-menu"
+                        href="/"
                         sx={{
                             mr: 2,
                             display: {xs: 'none', md: 'flex'},
@@ -105,7 +105,7 @@ function Header() {
                         variant="h5"
                         noWrap
                         component="a"
-                        href="#app-bar-with-responsive-menu"
+                        href="/"
                         sx={{
                             mr: 2,
                             display: {xs: 'flex', md: 'none'},
