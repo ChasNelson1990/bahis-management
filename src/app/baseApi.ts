433599229bbad2567405d0e373b8df5d7fdaf102
Sharful <sharful09@gmail.com>
2024-09-08 11:54:47 +0600

    fix: user url name

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -2,13 +2,10 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
     baseQuery: fetchBaseQuery({
-            baseUrl: "http://kf.sharful.com/api/v2/",
-            // baseUrl: "/api/v2/",
+            baseUrl: "/api/v2/",
             credentials: "include",
-            // mode: 'no-cors',
             prepareHeaders: (headers) => {
                 const token = localStorage.getItem('token');
-                // const token = `d7316cbff63921083545676fc9a3a9980d4355d6`;
                 headers.set("authorization", `TOKEN ${token}`);
                 headers.set("Content-Type", "application/json");
                 headers.set('Accept', 'application/json');
