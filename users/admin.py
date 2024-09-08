7ae464d5dc063a3acadc785950faede51a77e60c
Sharful <sharful09@gmail.com>
2024-09-08 15:28:42 +0600

    feat: add profile admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from bahis_management.desk.models import Module
+from users.models import Profile
 
 # Register your models here.
 admin.site.register(Module)
+admin.site.register(Profile)
